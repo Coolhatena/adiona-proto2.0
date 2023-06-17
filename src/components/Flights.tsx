3c183ce10e6f7b1db222a45937633f0860e5ad3a
mayra1703 <111815136+mayra1703@users.noreply.github.com>
2023-06-17 04:36:27 -0700

    formulario

--- src/components/Flights.tsx
+++ src/components/Flights.tsx
@@ -5,62 +5,39 @@ function Flights() {
                 <video className="vuelo contrast-100" autoPlay muted controls>
                     <source src="vuelo.mp4" type="video/mp4"/>
                 </video>
-                <div className="container_vuelo">
-                        <h1>Preparate para viajar!</h1>
-                        <button
-                          className="btn_login sign_in"
-                          type="submit">
-                          Sign in
-                        </button>
-                    </div>
             </div>
 
-            <form>
-                <div className="form-row">
-                    <div className="col-md-4 mb-3">
-                        <label>First name</label>
-                        <input type="text" className="form-control is-valid" id="validationServer01" placeholder="First name" value="Mark" required />
-                        <div className="valid-feedback">
-                            Looks good!
-                        </div>
+            <form className="pl-16 pr-16">
+                <h1 className="pt-4 pb-4">Reservacion de vuelo</h1>
+                <div className="form-row d-flex text-left">
+                    <div className="col m-4">
+                        <label className="pl-1">First name</label>
+                        <input type="text" className="form-control" id="validationServer01" placeholder="First name" value="Mark" required />
+                        
                     </div>
-                    <div className="col-md-4 mb-3">
-                        <label>Last name</label>
-                        <input type="text" className="form-control is-valid" id="validationServer02" placeholder="Last name" value="Otto" required />
-                        <div className="valid-feedback">
-                            Looks good!
-                        </div>
-                    </div>
-                    <div className="col-md-4 mb-3">
-                        <label>Username</label>
-                        <div className="input-group">
-                            <div className="input-group-prepend">
-                                <span className="input-group-text" id="inputGroupPrepend3">@</span>
-                            </div>
-                            <input type="text" className="form-control is-invalid" id="validationServerUsername" placeholder="Username" aria-describedby="inputGroupPrepend3" required />
-                            <div className="invalid-feedback">
-                                Please choose a username.
-                            </div>
-                        </div>
+                    <div className="col m-4">
+                        <label className="pl-1">Last name</label>
+                        <input type="text" className="form-control" id="validationServer01" placeholder="First name" value="Mark" required />
                     </div>
                 </div>
-                <div className="form-row">
-                    <div className="col-md-6 mb-3">
-                        <label>City</label>
+                
+                <div className="form-row text-left">
+                    <div className="col m-4 w-50">
+                        <label className="pl-1">City</label>
                         <input type="text" className="form-control is-invalid" id="validationServer03" placeholder="City" required />
                         <div className="invalid-feedback">
                             Please provide a valid city.
                         </div>
                     </div>
-                    <div className="col-md-3 mb-3">
-                        <label>State</label>
+                    <div className="col m-4 w-50">
+                        <label className="pl-1">State</label>
                         <input type="text" className="form-control is-invalid" id="validationServer04" placeholder="State" required />
                         <div className="invalid-feedback">
                             Please provide a valid state.
                         </div>
                     </div>
-                    <div className="col-md-3 mb-3">
-                        <label>Zip</label>
+                    <div className="col m-4 w-50">
+                        <label className="pl-1">Zip</label>
                         <input type="text" className="form-control is-invalid" id="validationServer05" placeholder="Zip" required />
                         <div className="invalid-feedback">
                             Please provide a valid zip.
@@ -68,7 +45,7 @@ function Flights() {
                     </div>
                 </div>
                 <div className="form-group">
-                    <div className="form-check">
+                    <div className="form-check col m-4 text-left">
                         <input className="form-check-input is-invalid" type="checkbox" value="" id="invalidCheck3" required />
                         <label className="form-check-label">
                             Agree to terms and conditions
@@ -78,7 +55,7 @@ function Flights() {
                         </div>
                     </div>
                 </div>
-                <button className="btn btn-primary" type="submit">Submit form</button>
+                <button className="btn_reservar" type="submit">Submit form</button>
             </form>
         </div>
     );
