32989bfddde33b40028ccf8de4a2b1473e6c6baa
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2023-06-16 20:23:03 -0700

    FINALLY fix polkadot button themes

--- src/components/layout/header/account/account-button/AccountButton.tsx
+++ src/components/layout/header/account/account-button/AccountButton.tsx
@@ -1,42 +1,58 @@
-import Identicon from '@polkadot/react-identicon';
-import clsx from 'clsx';
-import { buttonStyles } from '@gear-js/ui';
+import Identicon from "@polkadot/react-identicon";
+import clsx from "clsx";
+import { buttonStyles } from "@gear-js/ui";
 
 type Props = {
-  address: string;
-  name: string | undefined;
-  onClick: () => void;
-  isActive?: boolean;
-  block?: boolean;
+	address: string;
+	name: string | undefined;
+	onClick: () => void;
+	isActive?: boolean;
+	block?: boolean;
 };
 
 function AccountButton({ address, name, onClick, isActive, block }: Props) {
-  const className = clsx(
-    isActive ? buttonStyles.primary : buttonStyles.light,
-    block && buttonStyles.block,
-  );
+	const className = clsx(
+		isActive ? buttonStyles.primary : buttonStyles.light,
+		block && buttonStyles.block
+	);
 
-  const styles = {
+	const isDarkMode = () =>
+		window.matchMedia &&
+		window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+	const textColor = isDarkMode() ? "white" : "black";
+
+	const styles = {
 		"align-items": "center",
 		"border-radius": "24px",
-		"display": "inline-flex",
+		display: "inline-flex",
 		"font-weight": "600",
 		"justify-content": "space-between",
-		"transition": "all .25s",
+		transition: "all .25s",
 		"font-size": "14px",
 		"line-height": "16px",
 		"max-height": "40px",
-		"padding": "12px 24px",
-		"background-color": "hsla(0, 0%, 100%, .102)"
-	}
+		padding: "12px 24px",
+		"background-color": "hsla(0, 0%, 100%, .102)",
+		color: textColor,
+	};
 
-
-  return (
-    <button type="button" style={styles} onClick={onClick}>
-      <Identicon value={address} className={buttonStyles.icon} theme="polkadot" size={28} />
-      <p style={{marginLeft: "0.5rem"}}>{name}</p>
-    </button>
-  );
+	return (
+		<button
+			type="button"
+			style={styles}
+			className="light:text-black!important dark:text-white!important"
+			onClick={onClick}
+		>
+			<Identicon
+				value={address}
+				className={buttonStyles.icon}
+				theme="polkadot"
+				size={28}
+			/>
+			<p style={{ marginLeft: "0.5rem" }}>{name}</p>
+		</button>
+	);
 }
 
 export { AccountButton };
