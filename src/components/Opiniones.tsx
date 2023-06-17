12df80933538211bec121b0164e5fb30666c2d47
Eduardo Rivera Geffroy <ingsc.geffroy@gmail.com>
2023-06-16 18:24:39 -0700

    Fix dark mode double text

--- src/components/Opiniones.tsx
+++ src/components/Opiniones.tsx
@@ -1,6 +1,6 @@
 function Opiniones() {
     return(
-        <div className="d-inline-block">
+        <div className="dark:bg-gray-900 d-inline-block">
             <h2 className="testimonials">Testimonials</h2>
             <p className="mb-8 text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">Our travelers speak their minds, we love to hear them</p>
       
@@ -11,10 +11,6 @@ function Opiniones() {
                     </svg> 
                     
                     <blockquote className="pl-6 pr-6">
-                        <p className="pl-6 pr-6 text-xl text-justif font-medium text-gray-900">&#34;Adiona Travel Agency provided a stress-free and personalized vacation experience. Their attentive team crafted a perfect itinerary and arranged top-notch accommodations and activities. Highly recommended!&#34;</p>
-                    </blockquote>
-
-                    <blockquote>
                         <p className="pl-6 pr-6 text-xl text-justif font-medium text-gray-900 dark:text-white">&#34;Adiona Travel Agency provided a stress-free and personalized vacation experience. Their attentive team crafted a perfect itinerary and arranged top-notch accommodations and activities. Highly recommended!&#34;</p>
                     </blockquote>
                     
@@ -48,7 +44,7 @@ function Opiniones() {
                         <path d="M14.017 18L14.017 10.609C14.017 4.905 17.748 1.039 23 0L23.995 2.151C21.563 3.068 20 5.789 20 8H24V18H14.017ZM0 18V10.609C0 4.905 3.748 1.038 9 0L9.996 2.151C7.563 3.068 6 5.789 6 8H9.983L9.983 18L0 18Z" fill="currentColor"/>
                     </svg> 
                     <blockquote className="pl-6 pr-6">
-                        <p className="pl-6 pr-6 text-xl text-justif font-medium text-gray-900">&#34;Adiona Travel Agency made our honeymoon unforgettable. From airport transfers to surprise perks, their personalized service and meticulous planning were exceptional. Highly recommended!&#34;</p>
+                        <p className="pl-6 pr-6 text-xl text-justif font-medium text-gray-900 dark:text-white">&#34;Adiona Travel Agency made our honeymoon unforgettable. From airport transfers to surprise perks, their personalized service and meticulous planning were exceptional. Highly recommended!&#34;</p>
                     </blockquote>
                     <figcaption className="flex items-center justify-center mt-2 space-x-3">
                         <div className="flex items-center divide-x-2 divide-gray-500 dark:divide-gray-700">
